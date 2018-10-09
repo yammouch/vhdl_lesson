e144d5a6fec9b2c80cc6c7a566a316924bccbd31
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-10-09 19:09:09 +0900

    First checkin.

---
+++ 0130_architectures/0020_2files/sim_top.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sim_top is
+end entity;
+
+architecture sim1 of sim_top is
+begin
+  process
+  begin
+    report "sim1";
+    wait;
+  end process;
+end sim1;
