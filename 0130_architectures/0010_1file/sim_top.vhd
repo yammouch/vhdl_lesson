874bb83fc47027cf9cceb79850d5be1207d4f6bb
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-10-09 18:42:11 +0900

    First checkin.

---
+++ 0130_architectures/0010_1file/sim_top.vhd
@@ -0,0 +1,27 @@
+-- $ ghdl -a sim_top.vhd
+-- $ ghdl -e sim_top
+-- $ ghdl -r sim_top
+-- > sim2
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sim_top is
+end entity;
+
+architecture sim1 of sim_top is
+begin
+  process
+  begin
+    report "sim1";
+    wait;
+  end process;
+end sim1;
+
+architecture sim2 of sim_top is
+begin
+  process
+  begin
+    report "sim2";
+    wait;
+  end process;
+end sim2;
