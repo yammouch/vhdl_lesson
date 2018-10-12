e7482e1c974c234ec1fb2dea349d299753926c80
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-10-12 19:47:15 +0900

    Updated.

--- 0140_lfsr/ex01.vhd
+++ 0140_lfsr/ex01.vhd
@@ -15,20 +15,23 @@ architecture behav of ex01 is
   return std_ulogic_vector is
     variable retval : std_ulogic_vector(din'range);
   begin
-    retval := std_ulogic_vector(shift_right(unsigned(din), 1));
-    if din(0) = '1' then
-      for i in pow'range loop
-        retval := retval
-              xor std_ulogic_vector(shift_left( to_unsigned(1, din'length)
-                                              , pow(i) - 1 ) );
-      end loop;
+    if din = std_ulogic_vector(to_unsigned(0, din'length)) then
+      retval := (0 => '1', others => '0');
+    else
+      retval := std_ulogic_vector(shift_right(unsigned(din), 1));
+      if din(0) = '1' then
+        for i in pow'range loop
+          retval := retval
+                xor std_ulogic_vector(shift_left( to_unsigned(1, din'length)
+                                                , pow(i) - 1 ) );
+        end loop;
+      end if;
     end if;
     return retval;
   end function;
 
   signal   clk        : std_ulogic := '0';
-  signal   shift_reg  : std_ulogic_vector(7 downto 0) := x"01";
-  constant lfsr_coeff : nar(0 to 3) := (8, 6, 5, 4);
+  signal   shift_reg  : std_ulogic_vector(7 downto 0) := (others => '0');
 
 begin
 
@@ -52,8 +55,7 @@ begin
   process
   begin
     wait until clk'event and clk = '1';
-    --shift_reg <= lfsr(shift_reg, (8, 6, 5, 4));
-    shift_reg <= lfsr(shift_reg, lfsr_coeff);
+    shift_reg <= lfsr(shift_reg, (8, 6, 5, 4));
   end process;
 
 end behav;
